ea84751268dad842104298eb7d93e50139c22e94
ABHAY TARAS <abhaytaras18@gmail.com>
2024-03-21 12:32:22 +0530

    new

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,31 @@
-fun main() {
-    println("Hello World!")
+fun displayDataTypes(){
+
+    val memoryWanted : Byte = 50
+    val workerWanted : Short = 1902
+    val numberOfTanker : Int = 2147483647
+    val ownerName : String  = "Kian "
+    val isActualOwner : Boolean  = true
+    val netWorthOfGenerations : Long   = 9223372036854775807L
+    val ownerHeight : Float = 175.32F
+    val ownerPenisSize : Double = 8.98
+    val ownerGender : Char = 'M'
+
+    println(memoryWanted)
+    println(workerWanted)
+    println(numberOfTanker)
+    println(ownerName)
+    println(isActualOwner)
+    println(netWorthOfGenerations)
+    println(ownerHeight)
+    println(ownerPenisSize)
+    println(ownerGender)
+    println(memoryWanted.toInt())
 }
+fun main() {
+
+//    val breadObject = MoreBread()
+//    val ref = breadObject.callTheBreadcrums()
+displayDataTypes()
+
+}
+
--- src/moreBread.kt
+++ src/moreBread.kt
@@ -1,2 +1,20 @@
-class moreBread {
+class MoreBread {
+
+    var somecrums: String = " here we go some more crums  !!!!!!! "
+    var foolsBreadNumber: Byte = 0
+    var quantity: Int = 0
+
+    constructor() {
+        this.foolsBreadNumber = foolsBreadNumber
+    }
+
+    fun callTheBreadcrums() {
+
+        var trimmedCrums = somecrums.trim()
+
+        println(somecrums)
+        println("trimmedcrums = " + trimmedCrums)
+        println(this.foolsBreadNumber)
+    }
+
 }
